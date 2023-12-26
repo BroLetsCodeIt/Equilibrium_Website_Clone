6ed9ae5e99ed81dc586fd4413ac2146d49dce92e
kamleshkaparvena <kamleshkaparvena3570@gmail.com>
2023-12-26 14:47:03 +0530

    secondcommit

--- script.js
+++ script.js
@@ -58,8 +58,43 @@ Shery.imageEffect("#main .mask-target", {
   preset: "./presets/wigglewobble.json",
 });
 
+
+gsap.from('.ephermalimage img',{
+  z:-13,
+  duration:2,
+  opacity:0,
+})
+
 // Shery.imageEffect(".mask-target", {
 //     style: 6/*OR 5 for different variant */,
 //     debug: true,
 // });
   
+Shery.imageEffect('.leftpicimg ',{
+  style:5,
+  // debug:true,
+  gooey:true,
+  config:{"a":{"value":2,"range":[0,30]},"b":{"value":-0.76,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.5628742410104303},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":32}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
+})
+
+
+const video = document.querySelector('.video');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('mouseover',() =>{
+  gsap.to('video',{
+    opacity:1,
+    duration:3,
+    y:89
+  })
+  // video.style.opacity=1
+})
+
+btn.addEventListener('mouseleave',()=>{
+  gsap.to('video' ,{
+    opacity:0,
+    // display:none,
+    duration:2
+  })
+  // video.style.opacity = 0;
+})
